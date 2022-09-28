c7dd4e7e296077ccb82149fd18f3afbfe6e54fac
wisdomokubo <wisdomokubo@gmail.com>
2022-09-28 08:34:07 +0100

    my first commit
    Basic index page with buttons for all african countries and nigerian languages

---
+++ languages.js
@@ -0,0 +1,28 @@
+// JavaScript code 
+
+function search_language() { 
+
+    let input = document.getElementById('searchbar').value 
+
+    input=input.toLowerCase(); 
+
+    let x = document.getElementsByClassName('languages'); 
+
+      
+
+    for (i = 0; i < x.length; i++) {  
+
+        if (!x[i].innerHTML.toLowerCase().includes(input)) { 
+
+            x[i].style.display="none"; 
+
+        } 
+
+        else { 
+
+            x[i].style.display="list-item";                  
+
+        } 
+
+    } 
+} 
---
+++ nigerianlanguages.js
@@ -0,0 +1,28 @@
+// JavaScript code 
+
+function search_nigerianlanguage() { 
+
+    let input = document.getElementById('searchbarnigeria').value 
+
+    input=input.toLowerCase(); 
+
+    let x = document.getElementsByClassName('nigerianlanguages'); 
+
+      
+
+    for (i = 0; i < x.length; i++) {  
+
+        if (!x[i].innerHTML.toLowerCase().includes(input)) { 
+
+            x[i].style.display="none"; 
+
+        } 
+
+        else { 
+
+            x[i].style.display="list-item";                  
+
+        } 
+
+    } 
+} 
