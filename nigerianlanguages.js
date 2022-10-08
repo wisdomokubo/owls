ebfa69597ee51277ccf409dbfe7692e9a1272579
wisdomokubo <wisdomokubo@gmail.com>
2022-10-08 21:14:53 +0100

    latest commit
    Added Ikwerre to Nigerian languages.
    Created a test Degema language file.

--- nigerianlanguages.js
+++ nigerianlanguages.js
@@ -26,3 +26,31 @@ function search_nigerianlanguage() {
 
     } 
 } 
+
+// JavaScript code for Degema Language
+function search_degemalanguage() { 
+
+    let input = document.getElementById('searchbardegema').value 
+
+    input=input.toLowerCase(); 
+
+    let x = document.getElementsByClassName('degemalanguage'); 
+
+      
+
+    for (i = 0; i < x.length; i++) {  
+
+        if (!x[i].innerHTML.toLowerCase().includes(input)) { 
+
+            x[i].style.display="none"; 
+
+        } 
+
+        else { 
+
+            x[i].style.display="list-item";                  
+
+        } 
+
+    } 
+} 
